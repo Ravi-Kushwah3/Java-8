c931dcb2a15239ccc06810948bdc8195263d9391
Ravi Kushwah <113255019+Ravi-Kushwah3@users.noreply.github.com>
2025-07-02 17:49:00 +0530

    array logic ques.

---
+++ ArrayTest01.java
@@ -0,0 +1,85 @@
+package com.array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayTest01 {
+
+	private static int findBiggestNumber(int arr[]) {
+		int maxValue = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+
+			if (maxValue < arr[i]) {
+				maxValue = arr[i];
+			}
+		}
+		return maxValue;
+	}
+
+	private static int findSmallestNumber(int arr[]) {
+		int minValue = arr[0];
+		for (int i = 0; i < arr.length; i++) {
+
+			if (arr[i] < minValue) {
+				minValue = arr[i];
+			}
+		}
+		return minValue;
+	}
+
+	private static List<Integer> findDuplicateNumber(int arr[]) {
+		List<Integer> dupList = new ArrayList<Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+
+				if (arr[i] == arr[j]) {
+					dupList.add(arr[i]);
+					break;
+				}
+			}
+		}
+		return dupList;
+	}
+
+	private static List<Integer> removeDuplicate(int arr[]) {
+		List<Integer> removeList = new ArrayList<Integer>();
+
+		int newLength = 0;
+		for (int i = 0; i < arr.length; i++) {
+			boolean isDuplicate = false;
+
+			for (int j = 0; j < newLength; j++) {
+				if (arr[i] == removeList.get(j)) {
+					isDuplicate = true;
+					break;
+				}
+			}
+			if (!isDuplicate) {
+				newLength++;
+				removeList.add(arr[i]);
+			}
+
+		}
+		return removeList;
+	}
+
+	public static void main(String[] args) {
+
+		int arr[] = { 2, 5, 20, 7, 1, 3, 10, 5, 3 ,-8,-1};
+
+		int max = ArrayTest01.findBiggestNumber(arr);
+		System.out.println("Biggest number : " + max + "\n______________________");
+
+		int min = ArrayTest01.findSmallestNumber(arr);
+		System.out.println("\nSmallest number : " + min + "\n______________________");
+
+		List<Integer> dupList = ArrayTest01.findDuplicateNumber(arr);
+		System.out.println("\nDuplicate numbers : " + Arrays.toString(dupList.toArray()) + "\n______________________");
+
+		List<Integer> removeList = ArrayTest01.removeDuplicate(arr);
+		System.out.println("\nDuplicate numbers : " + Arrays.toString(removeList.toArray()));
+
+	}
+
+}
